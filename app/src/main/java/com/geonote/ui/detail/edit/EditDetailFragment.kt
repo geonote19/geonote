da942eb5bc7200c1d9c1836c85ce728a22c8ad9d
a_shelkovich <a_shelkovich@todes.by>
2019-11-22 22:13:35 +0300

    фрагмент редактирования (не падает)

--- app/src/main/java/com/geonote/ui/detail/edit/EditDetailFragment.kt
+++ app/src/main/java/com/geonote/ui/detail/edit/EditDetailFragment.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import com.geonote.BR
 import com.geonote.R
 import com.geonote.databinding.FragmentDetailBinding
+import com.geonote.databinding.FragmentDetailsEditBinding
 import com.geonote.ui.MainActivity
 import com.geonote.ui.base.BaseFragment
 
-class EditDetailFragment : BaseFragment<FragmentDetailBinding, EditDetailFragmentViewModel, MainActivity>() {
+class EditDetailFragment : BaseFragment<FragmentDetailsEditBinding, EditDetailFragmentViewModel, MainActivity>() {
     override val mViewModelClass = EditDetailFragmentViewModel::class.java
-    override val mLayoutId = R.layout.fragment_detail
+    override val mLayoutId = R.layout.fragment_details_edit
     override val mBindingVariable = BR.viewmodel
 
     private var mNoteId = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val args = DetailFragmentArgs.fromBundle(arguments!!)
+        val args = EditDetailFragmentArgs.fromBundle(arguments!!)
         mNoteId = args.noteId
     }
 
