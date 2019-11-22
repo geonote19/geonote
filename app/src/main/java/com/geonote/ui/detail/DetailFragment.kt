a7a9c36305bba127c1df0230e7ff90db6f36a595
mpaniushkina <m.paniushkina@softteco.com>
2019-11-22 21:43:18 +0300

    updated details ui

--- app/src/main/java/com/geonote/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/geonote/ui/detail/DetailFragment.kt
@@ -6,6 +6,7 @@ import com.geonote.R
 import com.geonote.databinding.FragmentDetailBinding
 import com.geonote.ui.MainActivity
 import com.geonote.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : BaseFragment<FragmentDetailBinding, DetailFragmentViewModel, MainActivity>() {
     override val mViewModelClass = DetailFragmentViewModel::class.java
@@ -18,6 +19,8 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailFragmentViewMod
         super.onCreate(savedInstanceState)
         val args = DetailFragmentArgs.fromBundle(arguments!!)
         mNoteId = args.noteId
+
+       // mapPreview.setOnClickListener(DetailsFragmen)
     }
 
     override fun setupViewModel(viewModel: DetailFragmentViewModel) {
